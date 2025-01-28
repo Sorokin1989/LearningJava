1aa056fc6f9678fdfc8c9bbb370d79cdb1c0e74b
Dmitrii <sorokindm1try@yandex.ru>
2025-01-28 11:18:57 +0300

    lesson36 readingFromFile_2.1

--- src/ru/sorokin/lessons/lesson36/ReadingFromFile.java
+++ src/ru/sorokin/lessons/lesson36/ReadingFromFile.java
@@ -2,6 +2,7 @@ package ru.sorokin.lessons.lesson36;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadingFromFile {
@@ -12,9 +13,14 @@ public class ReadingFromFile {
 
 
         Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
-        }
+       String line=scanner.nextLine();
+       String[]numbersString=line.split(" ");
+       int[] numbers=new int[3];
+       int counter=0;
+       for (String number:  numbersString) {
+           numbers[counter++] = Integer.parseInt(number);
+       }
+        System.out.println(Arrays.toString(numbers));
         scanner.close();
     }
 }
