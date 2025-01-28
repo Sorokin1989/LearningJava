412880059125a5db676827cf6b5f17623095c3af
Dmitrii <sorokindm1try@yandex.ru>
2025-01-28 11:42:42 +0300

    lesson36 readingFromFile_2.2

--- src/ru/sorokin/lessons/lesson36/ReadingFromFile.java
+++ src/ru/sorokin/lessons/lesson36/ReadingFromFile.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 
 public class ReadingFromFile {
     public static void main(String[] args) throws FileNotFoundException {
-        String separator = File.separator;
-        String path = separator + "Users" + separator + "Дмитрий" + separator + "OneDrive" + separator + "Рабочий стол" + separator + "File.txt";
-        File file = new File(path);
+
+
+        File file = new File("test2");
 
 
         Scanner scanner = new Scanner(file);
-       String line=scanner.nextLine();
-       String[]numbersString=line.split(" ");
-       int[] numbers=new int[3];
-       int counter=0;
-       for (String number:  numbersString) {
-           numbers[counter++] = Integer.parseInt(number);
-       }
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+        for (String number : numbersString) {
+            numbers[counter++] = Integer.parseInt(number);
+        }
         System.out.println(Arrays.toString(numbers));
         scanner.close();
     }
