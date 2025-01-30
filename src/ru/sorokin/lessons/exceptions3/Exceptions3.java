482de5788c187de9e004293ff2d662727692f2ae
Dmitrii <sorokindm1try@yandex.ru>
2025-01-30 17:15:27 +0300

    lesson39 Exception_3

---
+++ src/ru/sorokin/lessons/exceptions3/Exceptions3.java
@@ -0,0 +1,8 @@
+package ru.sorokin.lessons.exceptions3;
+
+public class Exceptions3 {
+    public static void main(String[] args) {
+        int[] arr=new int[2];
+        System.out.println(arr[2]);
+    }
+}
