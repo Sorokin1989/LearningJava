7e75918e273b4a28c4d436053544803f5a5f2606
Dmitrii <sorokindm1try@yandex.ru>
2025-01-31 20:10:27 +0300

    lesson42 1.3

--- src/ru/sorokin/lessons/lesson42/Test.java
+++ src/ru/sorokin/lessons/lesson42/Test.java
@@ -2,9 +2,10 @@ package ru.sorokin.lessons.lesson42;
 
 public class Test {
     public static void main(String[] args) {
-        Animal animal1 = new Animal(1);
-        Animal animal2 = new Animal(1);
-        System.out.println(animal1.equals(animal2));
+    String string1="Hello";
+    String string2="Hello";
+        System.out.println(string1.equals(string2));
+
 
 
     }
