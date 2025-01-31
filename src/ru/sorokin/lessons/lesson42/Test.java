68c73d91b56372cd42778657108a73fb6bc76a34
Dmitrii <sorokindm1try@yandex.ru>
2025-01-31 19:31:39 +0300

    lesson42 1.0

---
+++ src/ru/sorokin/lessons/lesson42/Test.java
@@ -0,0 +1,10 @@
+package ru.sorokin.lessons.lesson42;
+
+public class Test {
+    public static void main(String[] args) {
+        int x=1;
+        int y=1;
+        System.out.println(x==y);
+
+    }
+}
