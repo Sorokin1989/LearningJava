9bb5b49992ffd3916723f687277e4204df73866e
Dmitrii <sorokindm1try@yandex.ru>
2025-01-31 20:54:17 +0300

    lesson43 1.0

---
+++ src/ru/sorokin/lessons/lesson43/Test.java
@@ -0,0 +1,10 @@
+package ru.sorokin.lessons.lesson43;
+
+public class Test {
+    public static void main(String[] args) {
+        Electrocar electrocar=new Electrocar(1);
+        electrocar.start();
+        Electrocar.Battery battery=new Electrocar.Battery();
+
+    }
+}
