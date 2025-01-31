5d4b1303182557fa13528afadfe2b978147d2f09
Dmitrii <sorokindm1try@yandex.ru>
2025-01-31 20:00:52 +0300

    lesson42 1.2

--- src/ru/sorokin/lessons/lesson42/Test.java
+++ src/ru/sorokin/lessons/lesson42/Test.java
@@ -4,7 +4,8 @@ public class Test {
     public static void main(String[] args) {
         Animal animal1 = new Animal(1);
         Animal animal2 = new Animal(1);
-        System.out.println(animal1 == animal2);
+        System.out.println(animal1.equals(animal2));
+
 
     }
 }
@@ -12,6 +13,13 @@ public class Test {
         private int id;
         public Animal(int id) {
             this.id=id;
+
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Animal otherAnimal=(Animal) obj;
+            return this.id==otherAnimal.id;
         }
     }
 
