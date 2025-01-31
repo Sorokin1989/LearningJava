14a185b3e456a7109ded8b000eb48bc5533ea9c0
Dmitrii <sorokindm1try@yandex.ru>
2025-01-31 19:45:34 +0300

    lesson42 1.1

--- src/ru/sorokin/lessons/lesson42/Test.java
+++ src/ru/sorokin/lessons/lesson42/Test.java
@@ -2,9 +2,16 @@ package ru.sorokin.lessons.lesson42;
 
 public class Test {
     public static void main(String[] args) {
-        int x=1;
-        int y=1;
-        System.out.println(x==y);
+        Animal animal1 = new Animal(1);
+        Animal animal2 = new Animal(1);
+        System.out.println(animal1 == animal2);
 
     }
 }
+    class Animal {
+        private int id;
+        public Animal(int id) {
+            this.id=id;
+        }
+    }
+
