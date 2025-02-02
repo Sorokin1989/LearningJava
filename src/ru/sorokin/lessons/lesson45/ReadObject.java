912f4bba939b9306b5d9605e94eebf01cfa66597
Dmitrii <sorokindm1try@yandex.ru>
2025-02-02 13:56:33 +0300

    lesson46 1.1

--- src/ru/sorokin/lessons/lesson45/ReadObject.java
+++ src/ru/sorokin/lessons/lesson45/ReadObject.java
@@ -3,6 +3,7 @@ package ru.sorokin.lessons.lesson45;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
@@ -10,13 +11,15 @@ public class ReadObject {
             FileInputStream fis = new FileInputStream("people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
+            int personCount = ois.readInt();
+            Person[] people = new Person[personCount];
+            for (int i = 0; i < personCount; i++) {
+                people[i] = (Person) ois.readObject();
+            }
+            System.out.println(Arrays.toString(people));
 
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
 
             ois.close();
-            System.out.println(person1);
-            System.out.println(person2);
         } catch (IOException e) {
             e.printStackTrace();
 
