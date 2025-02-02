199dc5d26eb113e537c8ea9c2500e2e7f6954b99
Dmitrii <sorokindm1try@yandex.ru>
2025-02-02 13:25:22 +0300

    lesson45 1.0

---
+++ src/ru/sorokin/lessons/lesson45/ReadObject.java
@@ -0,0 +1,28 @@
+package ru.sorokin.lessons.lesson45;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ReadObject {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("people.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+
+            Person person1 = (Person) ois.readObject();
+            Person person2 = (Person) ois.readObject();
+
+            ois.close();
+            System.out.println(person1);
+            System.out.println(person2);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
