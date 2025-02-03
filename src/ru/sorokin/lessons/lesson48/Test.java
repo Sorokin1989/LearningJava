57d4af249f987ed599b67813ba69b62440d048d3
Dmitrii <sorokindm1try@yandex.ru>
2025-02-03 17:11:32 +0300

    lesson49 1.5

--- src/ru/sorokin/lessons/lesson48/Test.java
+++ src/ru/sorokin/lessons/lesson48/Test.java
@@ -2,11 +2,8 @@ package ru.sorokin.lessons.lesson48;
 
 public class Test {
     public static void main(String[] args) {
-   // Season season=Season.AUTUMN;
-    //Animal animal=Animal.CAT;
-     //   System.out.println(animal.name());
-Animal frog=Animal.valueOf("FROG");
-        System.out.println(frog.getTranslation());
+   Season winter=Season.WINTER;
+        System.out.println(winter.ordinal());
     }
 
 
