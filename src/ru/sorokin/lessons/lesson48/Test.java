d465cf25784de9c09e815fd2d50f912ffa111a66
Dmitrii <sorokindm1try@yandex.ru>
2025-02-03 17:09:27 +0300

    lesson49 1.4

--- src/ru/sorokin/lessons/lesson48/Test.java
+++ src/ru/sorokin/lessons/lesson48/Test.java
@@ -2,32 +2,11 @@ package ru.sorokin.lessons.lesson48;
 
 public class Test {
     public static void main(String[] args) {
-       //  Animal animal = Animal.DOG;
-       // System.out.println(animal);
-        //     switch (animal) {
-        //        case CAT:
-        //           System.out.println("it's a cat");
-        //         break;
-        //      case DOG:
-        //          System.out.println("it's a dog");
-        //          break;
-
-        //  }
-       Season season = Season.WINTER;
-        System.out.println(season.getTemperature());
-
-      //  System.out.println(season.getClass());
-
-
-        //   switch (season) {
-        //      case SUMMER:
-        //        System.out.println("it's warm outside");
-        //          break;
-        //      case WINTER:
-        //        System.out.println("it's cold outside");
-        //         break;
-        //   }
-
+   // Season season=Season.AUTUMN;
+    //Animal animal=Animal.CAT;
+     //   System.out.println(animal.name());
+Animal frog=Animal.valueOf("FROG");
+        System.out.println(frog.getTranslation());
     }
 
 
