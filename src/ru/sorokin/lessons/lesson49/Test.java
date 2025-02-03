505707dc7b5d947dc5bca101025f1a7f04e82c0d
Dmitrii <sorokindm1try@yandex.ru>
2025-02-03 20:03:51 +0300

    lesson49 1.5

---
+++ src/ru/sorokin/lessons/lesson49/Test.java
@@ -0,0 +1,14 @@
+package ru.sorokin.lessons.lesson49;
+
+public class Test {
+    public static void main(String[] args) {
+counter(3);
+    }
+    private static void counter(int n) {
+        System.out.println(n);
+        if(n==0) {
+            return;
+        }
+        counter(n-1);
+    }
+}
