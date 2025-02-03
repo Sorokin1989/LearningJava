ac97a1dc55b82dd3a98de055633a016383d052a2
Dmitrii <sorokindm1try@yandex.ru>
2025-02-03 20:31:31 +0300

    lesson49 1.6

--- src/ru/sorokin/lessons/lesson49/Test.java
+++ src/ru/sorokin/lessons/lesson49/Test.java
@@ -2,13 +2,12 @@ package ru.sorokin.lessons.lesson49;
 
 public class Test {
     public static void main(String[] args) {
-counter(3);
+        System.out.println(fac(4));
+
     }
-    private static void counter(int n) {
-        System.out.println(n);
-        if(n==0) {
-            return;
-        }
-        counter(n-1);
+    private static int fac(int n) {
+        if(n==1)
+            return 1;
+return n*fac(n-1);
     }
 }
