0053ffbd8658aa0197833dff39b20773c65648bf
Dmitrii <sorokindm1try@yandex.ru>
2025-02-05 12:25:05 +0300

    arraw_list 1.4

--- src/ru/sorokin/lessons/arraw_list/Test.java
+++ src/ru/sorokin/lessons/arraw_list/Test.java
@@ -1,20 +1,14 @@
 package ru.sorokin.lessons.arraw_list;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-        }
-        list.remove(5);
-        System.out.println(list);
-        //мы проводим много удалений из нашего листа
-        list=new LinkedList<>();
+
+
+
     }
 }
-
