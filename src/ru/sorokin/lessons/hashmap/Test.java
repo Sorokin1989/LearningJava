ba513a568ba1ad5195e2702dc271e4fb8ef483d2
Dmitrii <sorokindm1try@yandex.ru>
2025-02-10 09:30:01 +0300

    hashMap 1.0

---
+++ src/ru/sorokin/lessons/hashmap/Test.java
@@ -0,0 +1,21 @@
+package ru.sorokin.lessons.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<Integer, String> map=new HashMap<>();
+        map.put(1, "Один");
+        map.put(2,"Два");
+        map.put(3,"Три");
+        System.out.println(map);
+
+        map.put(3,"Другое значение для ключа три");
+        System.out.println(map);
+        System.out.println(map.get(1));
+        System.out.println(map.get(10));
+
+
+    }
+}
