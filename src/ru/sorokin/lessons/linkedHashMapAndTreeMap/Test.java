c9d63f12cd9c4e4520fef53d69f3e55e01f44bce
Dmitrii <sorokindm1try@yandex.ru>
2025-02-10 10:45:29 +0300

    linkedMapAndTreeMap 1.1

--- src/ru/sorokin/lessons/linkedHashMapAndTreeMap/Test.java
+++ src/ru/sorokin/lessons/linkedHashMapAndTreeMap/Test.java
@@ -1,20 +1,31 @@
 package ru.sorokin.lessons.linkedHashMapAndTreeMap;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Test {
     public static void main(String[] args) {
-        Map<String,String> translations=new HashMap<>();
+        Map<Integer,String> hashMap=new HashMap<>();// внутри не гарантируется порядок
 
-        translations.put("Собака","Dog");
-        translations.put("Слон", "Elephant");
-        translations.put("Кошка","Cat");
+        Map<Integer,String> linkedHashMap=new LinkedHashMap<>();// в каком порядке пары добавлены, в таком порядке и вернутся
 
-        for(Map.Entry entry: translations.entrySet()) {
-            System.out.println(entry.getKey()+":"+entry.getValue());
+        Map<Integer,String> treeMap=new TreeMap<>();// пары(ключ,значение) сортируются по ключу (естественный порядок)
+
+        testMap(hashMap);
+
+    }
+    public static void testMap (Map<Integer,String> map) {
+        map.put(39,"Bob");
+        map.put(12,"Mike");
+        map.put(78,"Tom");
+        map.put(0,"Tim");
+        map.put(1500,"Lewis");
+        map.put(7,"Bob");
+        for(Map.Entry<Integer,String> entry: map.entrySet()) {
+            System.out.println(entry.getKey()+ ":" + entry.getValue());
 
         }
-
     }
 }
