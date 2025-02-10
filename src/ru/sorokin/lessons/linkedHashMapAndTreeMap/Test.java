2c3f7af1ccf77a29de3f841da06574201738a28d
Dmitrii <sorokindm1try@yandex.ru>
2025-02-10 10:46:54 +0300

    linkedMapAndTreeMap 1.2

--- src/ru/sorokin/lessons/linkedHashMapAndTreeMap/Test.java
+++ src/ru/sorokin/lessons/linkedHashMapAndTreeMap/Test.java
@@ -13,7 +13,7 @@ public class Test {
 
         Map<Integer,String> treeMap=new TreeMap<>();// пары(ключ,значение) сортируются по ключу (естественный порядок)
 
-        testMap(hashMap);
+        testMap(linkedHashMap);
 
     }
     public static void testMap (Map<Integer,String> map) {
