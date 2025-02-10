c45543026226e44120bcb5723f0e3201b7e515d2
Dmitrii <sorokindm1try@yandex.ru>
2025-02-10 09:41:15 +0300

    hashMap 1.1

--- src/ru/sorokin/lessons/hashmap/Test.java
+++ src/ru/sorokin/lessons/hashmap/Test.java
@@ -9,12 +9,12 @@ public class Test {
         map.put(1, "Один");
         map.put(2,"Два");
         map.put(3,"Три");
-        System.out.println(map);
+        for(Map.Entry<Integer,String> entry: map.entrySet()) {
+            System.out.println(entry.getKey()+":" + entry.getValue());
 
-        map.put(3,"Другое значение для ключа три");
-        System.out.println(map);
-        System.out.println(map.get(1));
-        System.out.println(map.get(10));
+        }
+
+
 
 
     }
