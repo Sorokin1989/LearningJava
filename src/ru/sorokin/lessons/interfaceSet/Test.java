1fadeb4e3de006a295121d6ba50b43a9499ed849
Dmitrii <sorokindm1try@yandex.ru>
2025-02-10 13:41:54 +0300

    Set 1.1

--- src/ru/sorokin/lessons/interfaceSet/Test.java
+++ src/ru/sorokin/lessons/interfaceSet/Test.java
@@ -1,22 +1,23 @@
 package ru.sorokin.lessons.interfaceSet;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
         Set<String> hashSet=new HashSet<>();
-        Set<String> linkedHashSet=new HashSet<>();
+        Set<String> linkedHashSet=new LinkedHashSet<>();
         Set<String> treeSet=new TreeSet<>();
 
-        hashSet.add("Mike");
-        hashSet.add("Katy");
-        hashSet.add("Tom");
-        hashSet.add("George");
-        hashSet.add("Donald");
+        linkedHashSet.add("Mike");
+        linkedHashSet.add("Katy");
+        linkedHashSet.add("Tom");
+        linkedHashSet.add("George");
+        linkedHashSet.add("Donald");
 
-        for (String name: hashSet) {
+        for (String name: linkedHashSet) {
             System.out.println(name);
 
         }
