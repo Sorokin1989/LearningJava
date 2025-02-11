d81ba0053c0e3b24b47d31303045ce7482291a32
Dmitrii <sorokindm1try@yandex.ru>
2025-02-11 12:53:16 +0300

    hashcode 1.1

--- src/ru/sorokin/lessons/hashcodeAndEquals/Test.java
+++ src/ru/sorokin/lessons/hashcodeAndEquals/Test.java
@@ -7,18 +7,39 @@ import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-        Map<Integer,String> map=new HashMap<>();
-        Set<Integer> set=new HashSet<>();
+        Map<Person,String> map=new HashMap<>();
+        Set<Person> set=new HashSet<>();
 
-        map.put(1,"Один");
-        map.put(1,"Другое значение");
+        Person person1=new Person(1,"Mike");
+        Person person2=new Person(2,"Katy");
 
-        set.add(1);
-        set.add(1);
+        map.put(person1,"123");
+        map.put(person2,"123");
 
+        set.add(person1);
+        set.add(person2);
         System.out.println(map);
         System.out.println(set);
 
 
+
+
+    }
+}
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
