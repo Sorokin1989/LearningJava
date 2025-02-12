a54c923daa41b224ce6de20bb8212e7639de36f9
Dmitrii <sorokindm1try@yandex.ru>
2025-02-12 11:14:05 +0300

    comparator 1.2

--- src/ru/sorokin/lessons/comparator/Test.java
+++ src/ru/sorokin/lessons/comparator/Test.java
@@ -7,40 +7,44 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> animals=new ArrayList<>();
+        List<String> animals = new ArrayList<>();
 
         animals.add("cat");
         animals.add("dog");
         animals.add("frog");
+        animals.add("a");
+        animals.add("wdwfefffw");
         animals.add("bird");
 
-        Collections.sort(animals);
+        Collections.sort(animals, new StringLengthComparator());
         System.out.println(animals);
 
-        List<Integer> numbers=new ArrayList<>();
-        numbers.add(1);
-        numbers.add(500);
-        numbers.add(100);
-        numbers.add(5);
+        // List<Integer> numbers=new ArrayList<>();
+        //numbers.add(1);
+        //numbers.add(500);
+        //numbers.add(100);
+        //numbers.add(5);
 
-        Collections.sort(numbers);
-        System.out.println(numbers);
-
-
-
-
+        //Collections.sort(numbers);
+        //System.out.println(numbers);
 
     }
 }
+
 class StringLengthComparator implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-       /* o1>o2==>1;
-          o1<o2==>-1;
-          o1==02==>0
-        */
+        if (o1.length() > o2.length()) {
+            return 1;
+        } else if (o1.length() < o2.length()) {
+            return -1;
+        } else {
+            return 0;
 
+        }
 
     }
+
 }
+
