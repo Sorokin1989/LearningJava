5d042f06dd952d344aa89cb1a775f5897dbe22cb
Dmitrii <sorokindm1try@yandex.ru>
2025-02-12 12:09:12 +0300

    comparator 1.5

--- src/ru/sorokin/lessons/comparator/Test.java
+++ src/ru/sorokin/lessons/comparator/Test.java
@@ -1,5 +1,6 @@
 package ru.sorokin.lessons.comparator;
 
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,40 +8,50 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        //List<String> animals = new ArrayList<>();
-
-        //animals.add("cat");
-        //animals.add("dog");
-        //animals.add("frog");
-        //animals.add("a");
-        //animals.add("wdwfefffw");
-        //animals.add("bird");
-
-        //Collections.sort(animals, new StringLengthComparator());
-        //System.out.println(animals);
-
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(500);
-        numbers.add(100);
-        numbers.add(5);
-
-        Collections.sort(numbers, new Comparator<Integer>() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(3, "Mike"));
+        people.add(new Person(1, "Bob"));
+        people.add(new Person(2, "Katy"));
+        Collections.sort(people, new Comparator<Person>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                if (o1 > o2) {
-                    return -1;
-                } else if (o1 < o2) {
+            public int compare(Person o1, Person o2) {
+                if (o1.getId() > o2.getId()) {
                     return 1;
+                } else if (o1.getId() < o2.getId()) {
+                    return -1;
                 } else {
                     return 0;
+
                 }
             }
         });
-        System.out.println(numbers);
+        System.out.println(people);
+
+
     }
 }
 
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
 
 
 
