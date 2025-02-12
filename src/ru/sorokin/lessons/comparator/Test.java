3dfec054339ca46f224caac42bfc6afa3bd14126
Dmitrii <sorokindm1try@yandex.ru>
2025-02-12 11:26:51 +0300

    comparator 1.3

--- src/ru/sorokin/lessons/comparator/Test.java
+++ src/ru/sorokin/lessons/comparator/Test.java
@@ -7,26 +7,26 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> animals = new ArrayList<>();
+        //List<String> animals = new ArrayList<>();
 
-        animals.add("cat");
-        animals.add("dog");
-        animals.add("frog");
-        animals.add("a");
-        animals.add("wdwfefffw");
-        animals.add("bird");
+        //animals.add("cat");
+        //animals.add("dog");
+        //animals.add("frog");
+        //animals.add("a");
+        //animals.add("wdwfefffw");
+        //animals.add("bird");
 
-        Collections.sort(animals, new StringLengthComparator());
-        System.out.println(animals);
+        //Collections.sort(animals, new StringLengthComparator());
+        //System.out.println(animals);
 
-        // List<Integer> numbers=new ArrayList<>();
-        //numbers.add(1);
-        //numbers.add(500);
-        //numbers.add(100);
-        //numbers.add(5);
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(500);
+        numbers.add(100);
+        numbers.add(5);
 
-        //Collections.sort(numbers);
-        //System.out.println(numbers);
+        Collections.sort(numbers,new BackwardsIntegerComparator());
+        System.out.println(numbers);
 
     }
 }
@@ -47,4 +47,20 @@ class StringLengthComparator implements Comparator<String> {
     }
 
 }
+
+class BackwardsIntegerComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (o1 > o2) {
+            return -1;
+        } else if (o1 < o2) {
+            return 1;
+        } else {
+            return 0;
+
+        }
+    }
+}
+
 
