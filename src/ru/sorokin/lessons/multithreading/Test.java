b62d3487ce0a804eeb3d0cdcb38dbad46dd60152
Dmitrii <sorokindm1try@yandex.ru>
2025-02-13 15:38:57 +0300

    multithreading 1.3

--- src/ru/sorokin/lessons/multithreading/Test.java
+++ src/ru/sorokin/lessons/multithreading/Test.java
@@ -1,12 +1,15 @@
 package ru.sorokin.lessons.multithreading;
 
 public class Test {
-    public static void main(String[] args) {
-     MyThread myThread=new MyThread();
-     myThread.start();
-
-     MyThread myThread2=new MyThread();
-     myThread2.start();
+    public static void main(String[] args) throws InterruptedException {
+     //MyThread myThread=new MyThread();
+     //myThread.start();
+//
+     //MyThread myThread2=new MyThread();
+     //myThread2.start();
+        System.out.println("I am going to sleep");
+        Thread.sleep(3000);
+        System.out.println("I am awake!");
 
     }
 }
