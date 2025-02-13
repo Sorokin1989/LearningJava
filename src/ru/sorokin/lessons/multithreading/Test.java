0ea98c874685a0981d67ff8de8d119de2fbf0c12
Dmitrii <sorokindm1try@yandex.ru>
2025-02-13 15:32:00 +0300

    multithreading 1.2

--- src/ru/sorokin/lessons/multithreading/Test.java
+++ src/ru/sorokin/lessons/multithreading/Test.java
@@ -4,14 +4,16 @@ public class Test {
     public static void main(String[] args) {
      MyThread myThread=new MyThread();
      myThread.start();
-        System.out.println("Hello from main Thread");
+
+     MyThread myThread2=new MyThread();
+     myThread2.start();
 
     }
 }
 class MyThread extends Thread {
     public void run() {
         for(int i=0; i<1000;i++) {
-            System.out.println("Hello from my Thread");
+            System.out.println("Hello from my Thread"+i);
         }
     }
 }
