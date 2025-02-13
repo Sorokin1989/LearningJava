39d784c5a1ba4acea562b955d56be4f93ca8a722
Dmitrii <sorokindm1try@yandex.ru>
2025-02-13 14:17:01 +0300

    iterator 1.1

--- src/ru/sorokin/lessons/iterator/Test.java
+++ src/ru/sorokin/lessons/iterator/Test.java
@@ -1,17 +1,34 @@
 package ru.sorokin.lessons.iterator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list=new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
 
-        for (int x: list)
-            System.out.println(x);
+        Iterator<Integer> iterator = list.iterator();
+        int idx=0;
+
+        //Before java 5
+         while (iterator.hasNext()) {
+             System.out.println(iterator.next());
+             if(idx==1) {
+                 iterator.remove();
+             }
+
+             idx++;
+         }
+        System.out.println(list);
+        //Java 5
+       // for (int x : list) {
+           // list.remove(1);
+
+          //  System.out.println(x);
 
 
         }
