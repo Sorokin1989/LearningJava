e4a8d43a9ac3072a61a328ce60a07bafc59b6df9
Dmitrii <sorokindm1try@yandex.ru>
2025-02-16 15:50:46 +0300

    synchronized 1.3

--- src/ru/sorokin/lessons/synchronized_1/Test.java
+++ src/ru/sorokin/lessons/synchronized_1/Test.java
@@ -1,46 +1,67 @@
 package ru.sorokin.lessons.synchronized_1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Test {
     private int counter;
 
     public static void main(String[] args) throws InterruptedException {
-        Test test = new Test();
-        test.doWork();
+        new Worker().main();
 
 
     }
-public void increment() {
-        synchronized (this) {
-            counter++;
-        }
 }
-    public void doWork() throws InterruptedException {
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10000; i++)
-                    increment();
+
+class Worker {
+    Random random = new Random();
 
 
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10000; i++)
-                    increment();
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void addToList1() {
 
 
-            }
-        });
-        thread1.start();
-        thread2.start();
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        list1.add(random.nextInt(100));
 
-        thread1.join();
-        thread2.join();
 
-        System.out.println(counter);
+    }
 
+    public void addToList2() {
+
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        list2.add(random.nextInt(100));
+
+    }
+
+    public void work() {
+        for (int i = 0; i < 1000; i++) {
+            addToList1();
+            addToList2();
+        }
+    }
+
+    public void main() {
+        long before = System.currentTimeMillis();
+        work();
+
+        long after = System.currentTimeMillis();
+        System.out.println("Program took " + (after - before) + "ms to run");
+
+        System.out.println("List1: " + list1.size());
+        System.out.println("List2: " + list2.size());
 
 
     }
