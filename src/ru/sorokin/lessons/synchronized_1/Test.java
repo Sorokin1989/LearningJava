053d48abd15ddd9125ddc5bb474f85d7c62c328d
Dmitrii <sorokindm1try@yandex.ru>
2025-02-16 15:13:37 +0300

    synchronized 1.2

--- src/ru/sorokin/lessons/synchronized_1/Test.java
+++ src/ru/sorokin/lessons/synchronized_1/Test.java
@@ -9,8 +9,10 @@ public class Test {
 
 
     }
-public synchronized void increment() {
-        counter++;
+public void increment() {
+        synchronized (this) {
+            counter++;
+        }
 }
     public void doWork() throws InterruptedException {
         Thread thread1 = new Thread(new Runnable() {
