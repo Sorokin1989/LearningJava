327dc01c9f534df440c495811d6865160ca45796
Dmitrii <sorokindm1try@yandex.ru>
2025-02-19 12:35:49 +0300

    semaphore 1.2

--- src/ru/sorokin/lessons/Semaphore/Test.java
+++ src/ru/sorokin/lessons/Semaphore/Test.java
@@ -2,20 +2,21 @@ package ru.sorokin.lessons.Semaphore;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
 
         Connection connection = Connection.getConnection();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 200; i++) {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        connection.doWork();
+                        connection.work();
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -36,6 +37,7 @@ public class Test {
 class Connection {
     private static Connection connection = new Connection();
     private int connectionsCount;
+    private Semaphore semaphore = new Semaphore(10);
 
 
     private Connection() {
@@ -47,7 +49,20 @@ class Connection {
 
     }
 
-    public void doWork() throws InterruptedException {
+    public void work() throws InterruptedException {
+
+        semaphore.acquire();
+        try {
+            doWork();
+        } finally {
+            semaphore.release();
+
+        }
+
+
+    }
+
+    private void doWork() throws InterruptedException {
         synchronized (this) {
             connectionsCount++;
             System.out.println(connectionsCount);
