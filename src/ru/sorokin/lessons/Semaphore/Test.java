d05c5e4c38a6ff560181c8de0993df95ea525ae8
Dmitrii <sorokindm1try@yandex.ru>
2025-02-19 12:23:43 +0300

    semaphore 1.1

--- src/ru/sorokin/lessons/Semaphore/Test.java
+++ src/ru/sorokin/lessons/Semaphore/Test.java
@@ -1,27 +1,62 @@
 package ru.sorokin.lessons.Semaphore;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
-    public static void main(String[] args)  {
-        Semaphore semaphore=new Semaphore(3);
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        try {
-            semaphore.acquire();
-            semaphore.acquire();
-            semaphore.acquire();
+        Connection connection = Connection.getConnection();
 
-            System.out.println("All permits have been acquired");
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.doWork();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
 
-            semaphore.acquire();
-
-            System.out.println("Can't reach here...");
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+                }
+            });
         }
 
-        semaphore.release();
-        System.out.println(semaphore.availablePermits());
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+
+    }
+
+
+}
+
+class Connection {
+    private static Connection connection = new Connection();
+    private int connectionsCount;
+
+
+    private Connection() {
+
+    }
+
+    public static Connection getConnection() {
+        return connection;
+
+    }
+
+    public void doWork() throws InterruptedException {
+        synchronized (this) {
+            connectionsCount++;
+            System.out.println(connectionsCount);
+        }
+        Thread.sleep(5000);
+
+        synchronized (this) {
+            connectionsCount--;
+        }
+
     }
 }
