35e1fcd085a9561b73fc41ab0d88db8f99d6ebc2
Dmitrii <sorokindm1try@yandex.ru>
2025-02-20 13:33:16 +0300

    CallableAndFuture 1.1

--- src/ru/sorokin/lessons/CallableAndFuture/Test.java
+++ src/ru/sorokin/lessons/CallableAndFuture/Test.java
@@ -1,4 +1,39 @@
 package ru.sorokin.lessons.CallableAndFuture;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Test {
+
+    private static int result;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Starting");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Finished");
+                result = 5;
+
+
+            }
+        });
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(result);
+    }
+
 }
