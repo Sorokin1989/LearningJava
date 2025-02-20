9ee0ff5f6e80a4347c95395e0623b10566f41f05
Dmitrii <sorokindm1try@yandex.ru>
2025-02-20 14:06:51 +0300

    CallableAndFuture 1.3

--- src/ru/sorokin/lessons/CallableAndFuture/Test.java
+++ src/ru/sorokin/lessons/CallableAndFuture/Test.java
@@ -1,34 +1,37 @@
 package ru.sorokin.lessons.CallableAndFuture;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Random;
+import java.util.concurrent.*;
 
 public class Test {
 
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
-        executorService.submit(new Callable<Integer>() {
-
-
-            @Override
-            public Integer call() throws Exception {
-                System.out.println("Starting");
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Finished");
-                return 5;
-
-
+        Future<Integer> future=executorService.submit(() -> {
+            System.out.println("Starting");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
+            System.out.println("Finished");
+            Random random=new Random();
+            return random.nextInt(10);
+
+
         });
         executorService.shutdown();
+        try {
+            int result= future.get();
 
-        System.out.println(result);
+            System.out.println(result);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
