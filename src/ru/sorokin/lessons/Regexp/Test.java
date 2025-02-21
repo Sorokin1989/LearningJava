2abd87fd0058f34fd9981f9fccc72e981e026e14
Dmitrii <sorokindm1try@yandex.ru>
2025-02-21 14:51:45 +0300

    Regexp 1.0

---
+++ src/ru/sorokin/lessons/Regexp/Test.java
@@ -0,0 +1,9 @@
+package ru.sorokin.lessons.Regexp;
+
+public class Test {
+    public static void main(String[] args) {
+        String a="1234";
+        System.out.println(a.matches("1235"));
+
+    }
+}
