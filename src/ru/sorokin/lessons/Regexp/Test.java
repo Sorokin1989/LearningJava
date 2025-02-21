afaba0af1b1472edf44eb3fdc82106e16c7eae8a
Dmitrii <sorokindm1try@yandex.ru>
2025-02-21 14:56:37 +0300

    Regexp 1.0

--- src/ru/sorokin/lessons/Regexp/Test.java
+++ src/ru/sorokin/lessons/Regexp/Test.java
@@ -2,8 +2,15 @@ package ru.sorokin.lessons.Regexp;
 
 public class Test {
     public static void main(String[] args) {
-        String a="1234";
-        System.out.println(a.matches("1235"));
+
+        /*
+        \\d- одна цифра
+
+
+        */
+
+        String a="hello";
+        System.out.println(a.matches("hello"));
 
     }
 }
