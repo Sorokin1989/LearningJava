07f71043b2760ee8fc02d77a16b85293a127cf1f
Dmitrii <sorokindm1try@yandex.ru>
2025-02-24 15:11:32 +0300

    LambdaExpressions 1.7

--- src/ru/sorokin/lessons/LambdaExpressions/Test.java
+++ src/ru/sorokin/lessons/LambdaExpressions/Test.java
@@ -1,6 +1,7 @@
 package ru.sorokin.lessons.LambdaExpressions;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Test {
@@ -25,15 +26,14 @@ public class Test {
         //    }
         //});
 
-        list.sort((s1, s2) -> {
+        Comparator<String> comparator = ((s1, s2) -> {
             if (s1.length() > s2.length()) return 1;
             else if (s1.length() < s2.length()) return -1;
             else return 0;
 
-
         });
 
-
+        list.sort(comparator);
         System.out.println(list);
 
 
