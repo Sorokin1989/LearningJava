4fb5a915e959d01aa930074b846c3bbd7d585156
Dmitrii <sorokindm1try@yandex.ru>
2025-02-24 14:05:49 +0300

    LambdaExpressions 1.2

--- src/ru/sorokin/lessons/LambdaExpressions/Test.java
+++ src/ru/sorokin/lessons/LambdaExpressions/Test.java
@@ -1,7 +1,40 @@
 package ru.sorokin.lessons.LambdaExpressions;
 
+interface Executable {
+    void execut();
+}
+class Runner {
+    public void run(Executable e) {
+        e.execut();
+
+    }
+}
+class ExecutableImplementation implements Executable {
+
+    @Override
+    public void execut() {
+        System.out.println("Hello");
+
+    }
+}
+
+
+
 public class Test {
     public static void main(String[] args) {
-        Thread thread=new Thread(() -> System.out.println("Hello"));
+
+        Runner runner=new Runner();
+        runner.run(new ExecutableImplementation());
+
+        runner.run(new Executable() {
+            @Override
+            public void execut() {
+                System.out.println("Hello");
+            }
+        });
+
+        runner.run(()-> System.out.println("Hello"));
+
+
     }
 }
