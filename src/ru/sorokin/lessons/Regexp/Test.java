7138a1196aa648ba6a9881d65b907d1cdd65dd69
Dmitrii <sorokindm1try@yandex.ru>
2025-02-24 09:34:39 +0300

    Regexp 1.4

--- src/ru/sorokin/lessons/Regexp/Test.java
+++ src/ru/sorokin/lessons/Regexp/Test.java
@@ -5,12 +5,19 @@ public class Test {
 
         /*
         \\d- одна цифра
+        \\w-  [a-zA-Z]одна английская буква
         + - 1 или более
         * - 0 или более
         ? - 0 или 1 символов до
         (x|y|z) - одна строка из множества строк
 
         [a-zA-Z] - все английские буквы
+        [0-9] - \\d
+        [^abc] все кроме abc
+        . - любой символ
+        {2} - 2 символа до (\\d{2})
+        {2, } - 2 или более символов (\\d{2, })
+        {2,4} - от 2 до 4 символов (\\d{2,4})
 
 
 
@@ -25,6 +32,18 @@ public class Test {
         System.out.println(c.matches("(-|\\+)?\\d+"));
 
         String d="ghrh1324464";
-        System.out.println(d.matches("[a-zA-Z]*\\d+"));
+        System.out.println(d.matches("[a-zA-Z31]*\\d+"));
+        String e="helloc";
+        System.out.println(e.matches("[^abc]*"));
+
+        String url="http://www.google.com";
+        String uri2="http://www.yundex.ru";
+        String url3="Hello there";
+        System.out.println(url.matches("http://www\\..+\\.(com|ru)"));
+        System.out.println(uri2.matches("http://www\\..+\\.(com|ru)"));
+        System.out.println(url3.matches("http://www\\..+\\.(com|ru)"));
+
+        String f="12";
+        System.out.println(f.matches("\\d{2}"));
     }
 }
