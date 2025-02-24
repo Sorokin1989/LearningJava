a90c6eeaa3ab1c7ff68bd7cc3641fa1a9007bfc3
Dmitrii <sorokindm1try@yandex.ru>
2025-02-24 15:04:28 +0300

    LambdaExpressions 1.6

--- src/ru/sorokin/lessons/LambdaExpressions/Test.java
+++ src/ru/sorokin/lessons/LambdaExpressions/Test.java
@@ -1,36 +1,43 @@
 package ru.sorokin.lessons.LambdaExpressions;
 
-interface Executable {
-    int execut(int x,int y);
-}
-
-class Runner {
-    public void run(Executable e) {
-       int a= e.execut(10,15);
-        System.out.println(a);
-
-    }
-}
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("Goodbye");
+        list.add("a");
+        list.add("ab");
 
-        Runner runner = new Runner();
-        int a=1;
+        //list.sort(new Comparator<String>() {
+        //    @Override
+        //    public int compare(String o1, String o2) {
+        //        if (o1.length() > o2.length()) {
+        //            return 1;
+        //        } else if (o1.length() < o2.length()) {
+        //            return -1;
+        //        } else {
+        //            return 0;
+//
+        //        }
+        //    }
+        //});
 
-        runner.run(new Executable() {
-            @Override
-            public int execut(int x,int y) {
-                int a=2;
-                return x+y;
+        list.sort((s1, s2) -> {
+            if (s1.length() > s2.length()) {
+                return 1;
+            } else if (s1.length() < s2.length()) {
+                return -1;
+            } else {
+                return 0;
 
             }
         });
 
 
-        runner.run( (x,y)->x+ y);
-
+        System.out.println(list);
 
 
     }
