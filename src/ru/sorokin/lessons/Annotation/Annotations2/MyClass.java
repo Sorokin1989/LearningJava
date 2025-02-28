b8c655f0c3fb1aee74df892064093109b54d2656
Dmitrii <sorokindm1try@yandex.ru>
2025-02-28 14:50:14 +0300

    Annotation 1.3

---
+++ src/ru/sorokin/lessons/Annotation/Annotations2/MyClass.java
@@ -0,0 +1,8 @@
+package ru.sorokin.lessons.Annotation.Annotations2;
+
+public class MyClass {
+    @Deprecated
+    public void myMethod() {
+        System.out.println("Hello from MyMethod");
+    }
+}
---
+++ src/ru/sorokin/lessons/Annotation/Annotations2/Test.java
@@ -0,0 +1,8 @@
+package ru.sorokin.lessons.Annotation.Annotations2;
+
+public class Test {
+    public static void main(String[] args) {
+        MyClass myClass=new MyClass();
+        myClass.myMethod();
+    }
+}
