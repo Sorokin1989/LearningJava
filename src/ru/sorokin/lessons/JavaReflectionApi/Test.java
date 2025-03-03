613bfb7fc2622e598bb5cdadcd944505755676c2
Dmitrii <sorokindm1try@yandex.ru>
2025-03-03 12:10:41 +0300

    JavaReflectionApi 1.0

---
+++ src/ru/sorokin/lessons/JavaReflectionApi/Test.java
@@ -0,0 +1,25 @@
+package ru.sorokin.lessons.JavaReflectionApi;
+
+import java.lang.reflect.Field;
+
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Person person = new Person();
+        Class personClass = Person.class;
+        Class personClass2 = person.getClass();
+        Class personClass3 = Class.forName("ru.sorokin.lessons.JavaReflectionApi.Person");
+
+        //Method[] methods=personClass.getMethods();
+        //for(Method method: methods) {
+        //    System.out.println(method.getName() + ", " + method.getReturnType() + ", " +
+        //            Arrays.toString(method.getParameterTypes()))
+        //}
+//
+        Field[] fields = personClass.getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(field.getName() + ", " + field.getType());
+
+        }
+    }
+}
