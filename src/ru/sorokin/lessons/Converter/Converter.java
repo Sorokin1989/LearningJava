57abb72d91f9d01adb4ad71820cc0d0f3ec3a69b
Dmitrii <sorokindm1try@yandex.ru>
2025-03-10 12:28:37 +0300

    converter 1.0

---
+++ src/ru/sorokin/lessons/Converter/Converter.java
@@ -0,0 +1,16 @@
+package ru.sorokin.lessons.Converter;
+
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number!");
+        Scanner sc=new Scanner(System.in);
+        int choice=sc.nextInt();
+        System.out.println("Your number is: " + choice);
+
+
+
+    }
+}
