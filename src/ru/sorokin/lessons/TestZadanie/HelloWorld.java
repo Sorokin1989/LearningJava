4daf35afdee393989ace469318cd1d2cf076f38d
Dmitrii <sorokindm1try@yandex.ru>
2025-03-10 11:23:40 +0300

    Test 1.0

---
+++ src/ru/sorokin/lessons/TestZadanie/HelloWorld.java
@@ -0,0 +1,9 @@
+package ru.sorokin.lessons.TestZadanie;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name="Dima";
+        System.out.println("Hello, " + name);
+
+    }
+}
