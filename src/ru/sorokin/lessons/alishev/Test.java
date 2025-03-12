4cea79d76eb97ae4b4701aba46b7097a441f1410
Dmitrii <sorokindm1try@yandex.ru>
2025-03-12 15:10:00 +0300

    switch 1.1

--- src/ru/sorokin/lessons/alishev/Test.java
+++ src/ru/sorokin/lessons/alishev/Test.java
@@ -4,19 +4,37 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введи возраст: ");
-        int age = scanner.nextInt();
-        if (age == 0) {
-            System.out.println("Ты родился");
-        } else if (age == 7) {
-            System.out.println("Ты пошел в школу");
-        } else if (age == 18) {
-            System.out.println("Ты закончил школу");
-        } else {
-            System.out.println("Ни одно из условий не подошло");
-        }
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Введи возраст: ");
+        //int age = scanner.nextInt();
+        //if (age == 0) {
+        //    System.out.println("Ты родился");
+        //} else if (age == 7) {
+        //    System.out.println("Ты пошел в школу");
+        //} else if (age == 18) {
+        //    System.out.println("Ты закончил школу");
+        //} else {
+        //    System.out.println("Ни одно из условий не подошло");
+        //}
+//
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи возраст :");
+        int age = scanner.nextInt();
+
+        switch (age) {
+            case 0:
+                System.out.println("Ты родился");
+                break;
+            case 7:
+                System.out.println("Ты пошел в школу");
+                break;
+            case 18:
+                System.out.println("Ты закончил школу");
+                break;
+            default:
+                System.out.println("Ни одно из условий не подходит");
+        }
     }
 
 }
