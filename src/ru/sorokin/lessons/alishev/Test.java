e90316a0195d7b05aeaa079a28f284b48fb9a273
Dmitrii <sorokindm1try@yandex.ru>
2025-03-12 14:38:14 +0300

    break 1.0

--- src/ru/sorokin/lessons/alishev/Test.java
+++ src/ru/sorokin/lessons/alishev/Test.java
@@ -1,7 +1,5 @@
 package ru.sorokin.lessons.alishev;
 
-import java.util.Scanner;
-
 public class Test {
     public static void main(String[] args) {
         //Scanner scanner = new Scanner(System.in);
@@ -13,16 +11,25 @@ public class Test {
         //}
         //System.out.println("Вы ввели 5");
 
-        Scanner scanner = new Scanner(System.in);
-        int value;
-        do {
-            System.out.println("Введите 5: ");
-            value = scanner.nextInt();
+        //Scanner scanner = new Scanner(System.in);
+        //int value;
+        //do {
+        //    System.out.println("Введите 5: ");
+        //    value = scanner.nextInt();
+        //}
+        //while (value != 5);
+        //System.out.println("Вы ввели 5: ");
+//
+//
+
+        int i = 0;
+        while (true) {
+            if (i == 15)
+                break;
+            System.out.println(i);
+            i++;
         }
-        while (value != 5);
-        System.out.println("Вы ввели 5: ");
-
-
+        System.out.println("Мы вышли из цикла");
     }
 
 }
