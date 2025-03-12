b7b59c69a3bfa29c9546d58be538eed21b207729
Dmitrii <sorokindm1try@yandex.ru>
2025-03-12 14:49:51 +0300

    break 1.1

--- src/ru/sorokin/lessons/alishev/Test.java
+++ src/ru/sorokin/lessons/alishev/Test.java
@@ -22,14 +22,21 @@ public class Test {
 //
 //
 
-        int i = 0;
-        while (true) {
-            if (i == 15)
-                break;
-            System.out.println(i);
-            i++;
+        //int i = 0;
+        //while (true) {
+        //    if (i == 15)
+        //        break;
+        //    System.out.println(i);
+        //    i++;
+        //}
+        //System.out.println("Мы вышли из цикла");
+
+        for (int i = 0; i < 15; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("Это нечетное число " + i);
         }
-        System.out.println("Мы вышли из цикла");
     }
 
 }
