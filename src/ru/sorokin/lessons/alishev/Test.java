530455cddeb34eced602d6b9880f14de6f17c3fe
Dmitrii <sorokindm1try@yandex.ru>
2025-03-12 14:25:00 +0300

    ScannerAndWhile 1.2

--- src/ru/sorokin/lessons/alishev/Test.java
+++ src/ru/sorokin/lessons/alishev/Test.java
@@ -4,13 +4,25 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Введите 5: ");
+        //int value = scanner.nextInt();
+        //while (value != 5) {
+        //    System.out.println("Введи 5: ");
+        //    value = scanner.nextInt();
+        //}
+        //System.out.println("Вы ввели 5");
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 5: ");
-        int value = scanner.nextInt();
-        while (value != 5) {
-            System.out.println("Введи 5: ");
+        int value;
+        do {
+            System.out.println("Введите 5: ");
             value = scanner.nextInt();
         }
-        System.out.println("Вы ввели 5");
+        while (value != 5);
+        System.out.println("Вы ввели 5: ");
+
+
     }
+
 }
